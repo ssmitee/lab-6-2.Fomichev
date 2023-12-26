7d8aa23c6bf26643f04866b755052dd333b33863
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-26 23:08:15 +0200

    Добавьте файлы проекта.

---
+++ lab 6-2.Fomichev/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MatrixLibrary
+{
+    public class MatrixOperations
+    {
+        public static double CalculateSumOfMinMax(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] < min)
+                        min = matrix[i, j];
+
+                    if (matrix[i, j] > max)
+                        max = matrix[i, j];
+                }
+            }
+
+            double sum = min + max;
+
+            return sum;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        double[,] matrix = new double[rows, columns];
+
+        // Вводим значения элементов матрицы
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите элемент [{i}, {j}]: ");
+                matrix[i, j] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        double sum = MatrixLibrary.MatrixOperations.CalculateSumOfMinMax(matrix);
+
+        Console.WriteLine("Сумма максимального и минимального элементов матрицы: " + sum);
+    }
+}
